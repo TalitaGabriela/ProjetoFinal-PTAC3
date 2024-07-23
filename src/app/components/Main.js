dfc237ad7a13db8ea0c11f3f2a1abba448544c59
TalitaGabriela <talitagabriela1216@gmail.com>
2024-07-23 16:08:21 -0400

    Implementando useState e useEffect - Requisito #3

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,9 +1,54 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { listaDeLightsticks } from "../api/route";
 
 export default function Main() {
+  const [listaDeLightsticks, setListaDeLightsticks] = useState([]);
+  const [listComplete,setListComplete] = useState([]);
+  const [search,setSearch] = useState("")
+
+
+    useEffect( ()=> {
+      const getLightstick = async ()=>{
+        const response = await fetch("http://localhost:3000/api/");
+        const data = await response.json();
+
+        setListaDeLightsticks(data);
+        setListComplete(data);
+      }
+
+      getLightstick();
+    }, []);
+
+    const searchText = (text) => {
+      setSearch(text);
+  
+      if (text.trim() == ""){
+        setListaDeLightsticks(listComplete);
+        return
+      }
+  
+      const newList =  listaDeLightsticks.filter((lightstick) => 
+        lightstick.title.toUpperCase().trim().includes(search.toUpperCase().trim())
+      );
+      setListProduct(newList);   
+    }
+
+    if (listComplete[0] == null) {
+      return (
+      <main>
+      <p>Error 404</p>
+      </main>
+      );
+    }
+
   return (
+  <>
+  <div>
+        <input type = "text" value={search} placeholder="Pesquise o produto" onChange={(event) => searchText(event.target.value)}/>
+</div>
   <main>
     {listaDeLightsticks.map((lightstick) =>
       <div key={lightstick.id}>
@@ -18,5 +63,6 @@ export default function Main() {
       </div>
     )};
   </main>
+  </>
   );
 }
