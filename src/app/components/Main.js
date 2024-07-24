7f1276ee3d11953eb186825c59d8b1c81dc42c39
TalitaGabriela <talitagabriela1216@gmail.com>
2024-07-23 22:20:20 -0400

    Implementação de ordenação de lista de produtos e correção de erros nas buscas

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -7,75 +7,107 @@ import Image from "next/image";
 
 export default function Main() {
   const [listaDeLightsticks, setListaDeLightsticks] = useState([]);
-  const [listComplete,setListComplete] = useState([]);
-  const [search,setSearch] = useState("")
-  const [errorFetch,setErrorFetch] = useState(false);
+  const [listComplete, setListComplete] = useState([]);
+  const [search, setSearch] = useState("")
+  const [errorFetch, setErrorFetch] = useState(false);
 
 
-    useEffect( ()=> { 
-      const getLightstick = async ()=>{
-        try{
+  useEffect(() => {
+    const getLightstick = async () => {
+      try {
         const response = await fetch("http://localhost:3000/api/");
         const data = await response.json();
 
         setListaDeLightsticks(data);
         setListComplete(data);
-      }catch{
+      } catch {
         setErrorFetch(true);
       }
-    }   
-
-      getLightstick();
-    }, []);
-
-    const searchText = (text) => {
-      setSearch(text);
-  
-      if (text.trim() == ""){
-        setListaDeLightsticks(listComplete);
-        return
-      }
-  
-      const newList =  listaDeLightsticks.filter((lightstick) => 
-        lightstick.title.toUpperCase().trim().includes(search.toUpperCase().trim())
-      );
-      setListaDeLightsticks(newList);   
     }
 
-    if (errorFetch == true){
-      return( 
-      <ErrorGetData/>
+    getLightstick();
+  }, []);
+
+
+  const ordAz = () => {
+    let newList = [...listaDeLightsticks].sort((a, b) =>
+      a.titulo.localeCompare(b.titulo)
     );
+    setListaDeLightsticks(newList);
+  }
+
+  const ordZa = () => {
+    let newList = [...listaDeLightsticks].sort((a, b) =>
+      b.titulo.localeCompare(a.titulo)
+    );
+    setListaDeLightsticks(newList);
+
+  }
+
+  const ordBaratCaro = () => {
+    let newList = [...listaDeLightsticks].sort((a, b) => a.preco - b.preco);
+    setListaDeLightsticks(newList);
+  }
+
+  const ordCaroBarat = () => {
+    let newList = [...listaDeLightsticks].sort((a, b) => a.preco - b.preco);
+    newList = newList.reverse();
+    setListaDeLightsticks(newList);
+  }
+
+
+  const searchText = (text) => {
+    setSearch(text);
+
+    if (text.trim() == "") {
+      setListaDeLightsticks(listComplete);
+      return
     }
 
-    if (listComplete[0] == null) {
-      return (
+    const newList = listComplete.filter((lightstick) =>
+      lightstick.titulo.toUpperCase().trim().includes(search.toUpperCase().trim())
+    );
+    setListaDeLightsticks(newList);
+  }
+
+  if (errorFetch == true) {
+    return (
+      <ErrorGetData />
+    );
+  }
+
+  if (listComplete[0] == null) {
+    return (
       <main>
-      <Carregamento/>
+        <Carregamento />
       </main>
-      );
-    }
+    );
+  }
 
   return (
-  <>
-  <div>
-        <input type = "text" value={search} placeholder="Pesquise o produto" onChange={(event) => searchText(event.target.value)}/>
-</div>
-  <main>
-    {listaDeLightsticks.map((lightstick) =>
-      <div key={lightstick.id}>
-        <h1>{lightstick.titulo}</h1>
-        <Image src={lightstick.imagem}
-          width={100}
-          height={100} alt="lightstick image" />
-        <h2>{lightstick.preco} R$</h2>
-        <button >    
-          {/*Eu já havia utilizado o componente Link passando o ID do objeto para rota dinâmica pois queria testa se a página dinâmica funcionava.*/}
-          <Link href={`/lightstick/${lightstick.id}`}>Ver mais</Link>
-        </button>
+    <>
+      <div>
+        <input type="text" value={search} placeholder="Pesquise o produto" onChange={(event) => searchText(event.target.value)} />
+        <button onClick={ordAz}>A - Z</button>
+        <button onClick={ordZa}>Z - A</button>
+        <button onClick={ordBaratCaro}>Barato - Caro</button>
+        <button onClick={ordCaroBarat}>Caro - Barato</button>
       </div>
-    )};
-  </main>
-  </>
+      <main>
+        {listaDeLightsticks.map((lightstick) =>
+          <div key={lightstick.id}>
+            <h1>{lightstick.titulo}</h1>
+            <Image src={lightstick.imagem}
+              width={100}
+              height={100} alt="lightstick image" />
+            <h2>{lightstick.preco} R$</h2>
+            <button >
+              {/*Eu já havia utilizado o componente Link passando o ID do objeto para rota dinâmica pois queria testa se a página dinâmica funcionava.*/}
+              <Link href={`/lightstick/${lightstick.id}`}>Ver mais</Link>
+            </button>
+          </div>
+        )}
+      </main>
+    </>
   );
 }
